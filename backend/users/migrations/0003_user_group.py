1c8a70bc209e4ff171d67908b5820ae10c2e98be
Crowdbotics <team@crowdbotics.com>
2020-10-11 12:33:37 +0000

    Update models

--- backend/users/migrations/0003_user_group.py
+++ backend/users/migrations/0003_user_group.py
@@ -6,14 +6,16 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('course', '0001_initial'),
-        ('users', '0002_auto_20201010_1033'),
+        ("course", "0001_initial"),
+        ("users", "0002_auto_20201010_1033"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='user',
-            name='group',
-            field=models.ManyToManyField(blank=True, related_name='user_group', to='course.Group'),
+            model_name="user",
+            name="group",
+            field=models.ManyToManyField(
+                blank=True, related_name="user_group", to="course.Group"
+            ),
         ),
     ]
